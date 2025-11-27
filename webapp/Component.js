a3c12bf25677fffc2d4dc819d7d73b9d28bf3356
karinaAgz <karinaangon10@gmial.com>
2025-11-27 22:16:13 +0000

    cambios

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,26 +1,29 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
     "logaligroup/etiquetas/model/models"
-], (UIComponent, models) => {
+], function (UIComponent, JSONModel, models) {
     "use strict";
 
     return UIComponent.extend("logaligroup.etiquetas.Component", {
-        metadata: {
-            manifest: "json",
-            interfaces: [
-                "sap.ui.core.IAsyncContentCreation"
-            ]
+        metadata : {
+            manifest: "json"
         },
 
-        init() {
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
-            this.setModel(models.createDeviceModel(), "device");
-
             // enable routing
             this.getRouter().initialize();
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
         }
     });
 });
